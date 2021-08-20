993b0cb29d11f8e08441a627847d231b6c4f453d
jdy0120 <jdy0120120@gmail.com>
2021-08-20 17:45:43 +0900

    [ADD]menu-animation

---
+++ menu-animation/app.js
@@ -0,0 +1,13 @@
+const target = document.querySelector(".target");
+
+const links = document.querySelectorAll(".mynav a");
+const colors = [
+  "deepblue",
+  "skyblue",
+  "orange",
+  "black",
+  "green",
+  "gold",
+  "magenta",
+  "darkblue",
+];
